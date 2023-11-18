d73c004624db0c591cbbc3d1e97eb74b878a1a62
love-to-code108 <love.to.code108@gmail.com>
2023-11-18 18:38:47 +0530

    working on making the signup page and the login page currently hope everything works out

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import React from 'react';
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 
 const signup = () => {
+
+
+  
   return (
-    <div>signup</div>
+    <div>
+      {/* SIGN UP WRITING */}
+
+      {/* THE BOX FOR THE FORM */}
+
+        {/* ENTER EMAIL */}
+
+
+    </div>
   )
 }
 
